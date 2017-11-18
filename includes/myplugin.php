b14d7c52117ae59bafbcf66f677c665ed850dde2
VDMNS <myskaype@gmail.com>
2017-11-18 04:25:22 +0200

    плагин

---
+++ includes/myplugin.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+namespace includes;
+
+class myplugin
+{
+    private static $instance = null;
+    private function __construct() {
+    }
+    public static function getInstance() {
+
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+
+    }
+}
+myplugin::getInstance();
+}
+
+static public function activation()
+    {
+        // debug.log
+        error_log('plugin '.myplugin.' activation');
+    }
+
+    static public function deactivation()
+    {
+        // debug.log
+        error_log('plugin '.STEPBYSTEP_PlUGIN_NAME.' deactivation');
+    }
+}
+StepByStepPlugin::getInstance();
