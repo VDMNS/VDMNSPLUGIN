9e7e54f921b53aea69502950cb8cf81aad1e1cab
VDMNS <myskaype@gmail.com>
2017-11-23 02:32:52 +0200

    поменял конфиги

--- config-path.php
+++ config-path.php
@@ -1,17 +1,18 @@
 <?php
 
-define("STEPBYSTEP_PlUGIN_DIR", plugin_dir_path(__FILE__));
-define("STEPBYSTEP_PlUGIN_URL", plugin_dir_url( __FILE__ ));
-define("STEPBYSTEP_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(STEPBYSTEP_PlUGIN_DIR)));
-define("STEPBYSTEP_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', STEPBYSTEP_PlUGIN_SLUG ));
-define("STEPBYSTEP_PlUGIN_OPTION_VERSION", STEPBYSTEP_PlUGIN_SLUG.'_version');
-define("STEPBYSTEP_PlUGIN_OPTION_NAME", STEPBYSTEP_PlUGIN_SLUG.'_options');
-define("STEPBYSTEP_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
+define("MY_PlUGIN_DIR", plugin_dir_path(__FILE__));
+define("MY_PlUGIN_URL", plugin_dir_url( __FILE__ ));
+define("MY_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(MY_PlUGIN_DIR)));
+define("MY_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', MY_PlUGIN_SLUG ));
+define("MY_PlUGIN_OPTION_VERSION", MY_PlUGIN_SLUG.'_version');
+define("MY_PlUGIN_OPTION_NAME", MY_PlUGIN_SLUG.'_options');
+define("MY_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
 
 if ( ! function_exists( 'get_plugins' ) ) {
     require_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
-$TPOPlUGINs = get_plugin_data(STEPBYSTEP_PlUGIN_DIR.'/'.basename(STEPBYSTEP_PlUGIN_DIR).'.php', false, false);
+$TPOPlUGINs = get_plugin_data(MY_PlUGIN_DIR.'/'.basename(MY_PlUGIN_DIR).'.php', false, false);
 
-define("STEPBYSTEP_PlUGIN_VERSION", $TPOPlUGINs['Version']);
-define("STEPBYSTEP_PlUGIN_NAME", $TPOPlUGINs['Name']);
+define("MY_PlUGIN_VERSION", $TPOPlUGINs['Version']);
+define("MY_PlUGIN_NAME", $TPOPlUGINs['Name']);
+define("MY_PlUGIN_DIR_LOCALIZATION", plugin_basename(MY_PlUGIN_DIR.'/languages/'));
