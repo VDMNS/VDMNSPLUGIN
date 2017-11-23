7ed20087684f24af1e6eefec63ebe2b62c37faa7
Пользователь <>
2017-11-23 16:13:28 +0200

    Автоматическая загрузка классов

--- includes/common/VdmnsAutoload.php
+++ includes/common/VdmnsAutoload.php
@@ -1,23 +1,25 @@
 <?php
 
 namespace includes\common;
-class MyPluginAutoload
+class VdmnsAutoload
 {
     private static $instance = null;
     private function __construct() {
         spl_autoload_register(array($this, 'autoloadNamespace'));
     }
+
     public static function getInstance(){
         if ( null == self::$instance ) {
             self::$instance = new self;
         }
         return self::$instance;
     }
+
     /**
      * @param $className
      */
     public function autoloadNamespace($className){
-        $fileClass = MYPLUGIN_PlUGIN_DIR.'/'.str_replace("\\","/",$className).'.php';
+        $fileClass = VDMNS_PlUGIN_DIR.'/'.str_replace("\\","/",$className).'.php';
         if (file_exists($fileClass)) {
             if (!class_exists($fileClass, FALSE)) {
                 require_once $fileClass;
@@ -25,4 +27,4 @@ class MyPluginAutoload
         }
     }
 }
-MyPluginAutoload::getInstance();
+VdmnsAutoload::getInstance();
