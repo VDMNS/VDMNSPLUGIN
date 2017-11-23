2f2d42ea060e2169e583ef8d804b9faced460432
Пользователь <>
2017-11-23 16:12:05 +0200

    Автоматическая загрузка классов: подключил к основному файлови плагина

---
+++ vdmns-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: my Plugin
+Plugin URI: 
+Description: my.
+Version: 1.0
+Author: vadimmanzhos
+Author URI: 
+Text Domain: 
+Domain Path: 
+*/
+require_once dirname(__FILE__).'/includes/VdmnsPlugin.php';
+require_once VDMNS_PlUGIN_DIR.'/includes/common/VdmnsAutoload.php';
+
+require_once plugin_dir_path(__FILE__) . '/config-path.php';
+
+
+register_activation_hook( __FILE__, array('includes\VdmnsPlugin' ,  'activation' ) );
+register_deactivation_hook( __FILE__, array('includes\VdmnsPlugin' ,  'deactivation' ) );
