4f62caf154b0726f55a9d0d3f52ac4398195c7b6
Пользователь <>
2017-11-23 16:09:45 +0200

    Исправлено

--- config-path.php
+++ config-path.php
@@ -1,18 +1,15 @@
 <?php
-
-define("MY_PlUGIN_DIR", plugin_dir_path(__FILE__));
-define("MY_PlUGIN_URL", plugin_dir_url( __FILE__ ));
-define("MY_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(MY_PlUGIN_DIR)));
-define("MY_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', MY_PlUGIN_SLUG ));
-define("MY_PlUGIN_OPTION_VERSION", MY_PlUGIN_SLUG.'_version');
-define("MY_PlUGIN_OPTION_NAME", MY_PlUGIN_SLUG.'_options');
-define("MY_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
-
+define("VDMNS_PlUGIN_DIR", plugin_dir_path(__FILE__));
+define("VDMNS_PlUGIN_URL", plugin_dir_url( __FILE__ ));
+define("VDMNS_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(VDMNS_PlUGIN_DIR)));
+define("VDMNS_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', VDMNS_PlUGIN_SLUG ));
+define("VDMNS_PlUGIN_OPTION_VERSION", VDMNS_PlUGIN_SLUG.'_version');
+define("VDMNS_PlUGIN_OPTION_NAME", VDMNS_PlUGIN_SLUG.'_options');
+define("VDMNS_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
 if ( ! function_exists( 'get_plugins' ) ) {
     require_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
-$TPOPlUGINs = get_plugin_data(MY_PlUGIN_DIR.'/'.basename(MY_PlUGIN_DIR).'.php', false, false);
+$TPOPlUGINs = get_plugin_data(VDMNS_PlUGIN_DIR.'/'.basename(VDMNS_PlUGIN_DIR).'.php', false, false);
 
-define("MY_PlUGIN_VERSION", $TPOPlUGINs['Version']);
-define("MY_PlUGIN_NAME", $TPOPlUGINs['Name']);
-define("MY_PlUGIN_DIR_LOCALIZATION", plugin_basename(MY_PlUGIN_DIR.'/languages/'));
+define("VDMNS_PlUGIN_VERSION", $TPOPlUGINs['Version']);
+define("VDMNS_PlUGIN_NAME", $TPOPlUGINs['Name']);
