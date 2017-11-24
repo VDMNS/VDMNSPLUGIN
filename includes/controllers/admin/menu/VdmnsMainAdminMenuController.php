56ffb85f1f945e3894324dd2e4a95eabb394eb7d
VDMNS <myskaype@gmail.com>
2017-11-24 22:48:25 +0200

    будет отвечать за главное меню плагина.

---
+++ includes/controllers/admin/menu/VdmnsMainAdminMenuController.php
@@ -0,0 +1,48 @@
+<?php
+namespace includes\controllers\admin\menu;
+
+
+class VdmnsAdminMenuController extends VdmnsBaseAdminMenuController
+{
+
+    public function action()
+    {
+        // TODO: Implement action() method.
+        /**
+         * add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+         *
+         */
+        $pluginPage = add_menu_page(
+            _x(
+                'Vdmns plugin'
+                'admin menu page' ,
+                VDMNS_PlUGIN_TEXTDOMAIN
+            ),
+            _x(
+                'Vdmns plugin',
+                'admin menu page' ,
+                VDMNS_PlUGIN_TEXTDOMAIN
+            ),
+            'manage_options',
+            VDMNS_PlUGIN_TEXTDOMAIN,
+            array(&$this,'render'),
+            VDMNS_PlUGIN_URL .'assets/images/main-menu.png'
+        );
+    }
+
+    /**
+     * Метод отвечающий за контент страницы
+     */
+    public function render()
+    {
+        // TODO: Implement render() method.
+	_e("Hello world", VDMNS_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
