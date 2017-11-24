23cdbc6e4a5d854d244b3dd20ffa7020b8b5bfbd
VDMNS <myskaype@gmail.com>
2017-11-24 23:23:34 +0200

    отвечает за добавление дочерней страницы  главному меню плагина.

---
+++ includes/controllers/admin/menu/VdmnsMainAdminSubMenuController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace includes\controllers\admin\menu;
+
+
+class VdmnsMainAdminSubMenuController extends VdmnsBaseAdminMenuController
+{
+
+    public function action()
+    {
+        // TODO: Implement action() method.
+        $plugin_page = add_submenu_page(
+            VDMNS_PlUGIN_TEXTDOMAIN,
+            _x(
+                'Sub Step By Step',
+                'admin menu page' ,
+                VDMNS_PlUGIN_TEXTDOMAIN
+            ),
+            _x(
+                'Sub Step By Step',
+                'admin menu page' ,
+                VDMNS_PlUGIN_TEXTDOMAIN
+            ),
+            'manage_options',
+            'vdmns_control_sub_menu',
+            array(&$this, 'render'));
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello world sub menu", VDMNS_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
