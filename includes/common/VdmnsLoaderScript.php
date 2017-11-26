305b052778850e7f9bc8831543a153489e896b41
VDMNS <myskaype@gmail.com>
2017-11-26 19:34:19 +0200

    стили

--- includes/common/VdmnsLoaderScript.php
+++ includes/common/VdmnsLoaderScript.php
@@ -42,6 +42,25 @@ class VdmnsLoaderScript
          * Зависимые скрипты добавляются автоматически.
          */
         wp_enqueue_script(VDMNS_PlUGIN_SLUG.'-AdminMain');
+		
+		
+		 /**
+         * wp_register_style( $handle, $src, $deps, $ver, $media );
+         * Регистрирует CSS файл в WordPress. После регистрации файл можно добавить в html документ с помощью
+         * функции wp_enqueue_style().
+         *
+         */
+        wp_register_style(
+            VDMNS_PlUGIN_SLUG.'-AdminMain', //$handle
+            VDMNS_PlUGIN_URL.'assets/admin/css/StepByStepAdminMain.css', // $src
+            array(), //$deps,
+            VDMNS_PlUGIN_VERSION // $ver
+        );
+        /**
+         * Правильно добавляет файл CSS стилей. Регистрирует файл стилей, если он еще не был зарегистрирован.
+         */
+        wp_enqueue_style(VDMNS_PlUGIN_SLUG.'-AdminMain');
+    }
 
 	}
     public function loadHeadScriptAdmin(){}
