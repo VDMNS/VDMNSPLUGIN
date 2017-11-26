c0f3ad5ad06ca3e51d1c9355daaa80464b7cc3ba
VDMNS <myskaype@gmail.com>
2017-11-26 19:53:15 +0200

    удалил

--- includes/common/VdmnsLoader.php
+++ includes/common/VdmnsLoader.php
@@ -49,17 +49,7 @@ class VdmnsLoader
      */
     public function admin(){
 		VdmnsMainAdminMenuController::newInstance();
-		VdmnsMainAdminSubMenuController::newInstance();
-        VdmnsMyDashboardMenuController::newInstance();
-        VdmnsMyPostsMenuController::newInstance();
-        VdmnsMyMediaMenuController::newInstance();
-        VdmnsMyPagesMenuController::newInstance();
-        VdmnsMyCommentsMenuController::newInstance();
-        VdmnsMyThemeMenuController::newInstance();
-        VdmnsMyPluginsMenuController::newInstance();
-        VdmnsMyUsersMenuController::newInstance();
-        VdmnsMyToolsMenuController::newInstance();
-        VdmnsMyOptionsMenuController::newInstance();
+		
     }
     /**
      * Метод будет срабатывать когда вы находитесь Сайте. Загрузка классов для Сайта
