1bad9b4a623397fddfd6569f233cad6383d752f3
VDMNS <myskaype@gmail.com>
2017-11-26 18:36:59 +0200

    Добавить дочернюю страницу меню в меню Users

---
+++ includes/controllers/admin/menu/VdmnsMyUsersMenuController.php
@@ -0,0 +1,36 @@
+
+<?php
+
+
+namespace includes\controllers\admin\menu;
+
+
+class VdmnsMyUsersMenuController extends VdmnsBaseAdminMenuController
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+
+        $pluginPage = add_users_page(
+            __('Sub users Step By Step', VDMNS_PlUGIN_TEXTDOMAIN),
+            __('Sub users Step By Step', VDMNS_PlUGIN_TEXTDOMAIN),
+            'read',
+            'vdmns_control_sub_users_menu',
+            array(&$this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello this page Users", VDMNS_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
+
