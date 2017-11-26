50c780bfed23e039d85687a06831e4173a4da513
VDMNS <myskaype@gmail.com>
2017-11-26 17:45:36 +0200

    Добавить дочернюю страницу меню в меню Options

---
+++ includes/controllers/admin/menu/VdmnsMyOptionsMenuController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace includes\controllers\admin\menu;
+
+
+class VdmnsMyOptionsMenuController  extends VdmnsBaseAdminMenuController
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+
+        $pluginPage = add_management_page(
+            __('Sub tools Step By Step', VDMNS_PlUGIN_TEXTDOMAIN),
+            __('Sub tools Step By Step', VDMNS_PlUGIN_TEXTDOMAIN),
+            'read',
+            'vdmns_control_sub_options_menu',
+            array(&$this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello this page Tools", VDMNS_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
