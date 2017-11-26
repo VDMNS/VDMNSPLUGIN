cc9365c23e4ce5bb29dfaed4bee62924b65d838a
VDMNS <myskaype@gmail.com>
2017-11-26 18:38:04 +0200

    Добавить  страницу меню

---
+++ includes/controllers/admin/menu/VdmnsPluginsMenuController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace includes\controllers\admin\menu;
+
+
+class VdmnsPluginsMenuController extends VdmnsBaseAdminMenuController
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+
+        $pluginPage = add_plugins_page(
+            __('Sub plugins Step By Step', VDMNS_PlUGIN_TEXTDOMAIN),
+            __('Sub plugins Step By Step', VDMNS_PlUGIN_TEXTDOMAIN),
+            'read',
+            'vdmns_control_sub_plugins_menu',
+            array(&$this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello this page Plugins", VDMNS_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
