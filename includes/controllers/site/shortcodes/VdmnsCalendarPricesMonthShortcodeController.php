a773392804461eba05613215ac444e7515b2d70f
VDMNS <myskaype@gmail.com>
2017-12-04 23:46:38 +0200

    Теперь мы можем сделать шорткод для метода API “Календарь цен на месяц”. Создадим для этого класс

---
+++ includes/controllers/site/shortcodes/VdmnsCalendarPricesMonthShortcodeController.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace includes\controllers\site\shortcodes;
+
+
+use includes\common\VdmnsRequestApi;
+use includes\controllers\admin\menu\VdmnsIСreatorInstance;
+
+class VdmnsCalendarPricesMonthShortcodeController extends VdmnsShortcodesController
+    implements VdmnsIСreatorInstance
+{
+
+    /**
+     * Функция в которой будем добалять шорткоды через функцию add_shortcode( $tag , $func );
+     * @return mixed
+     */
+    public function initShortcode()
+    {
+        // TODO: Implement initShortcode() method.
+        /*
+         * Добавляем щорткод [step_by_step_calendar_price_month]
+         */
+        add_shortcode( 'vdmns_calendar_price_month', array(&$this, 'action'));
+    }
+
+    /**
+     * Функция обработки шоткода
+     * Функция указанная в параметре $func, получает 3 параметра, каждый из них может быть передан,
+     * а может нет:
+     * $atts(массив)
+     *      Ассоциативный массив атрибутов указанных в шоткоде. По умолчанию пустая строка - атрибуты
+     *      не переданы.
+     *      По умолчанию: ''
+     * $content(строка)
+     *      Текст шоткода, когда используется закрывающая конструкция шотркода: [foo]текст шорткода[/foo]
+     *      По умолчанию: ''
+     * $tag(строка)
+     *      Тег шорткода. Может пригодится для передачи в доп. функции. Пр: если шорткод - [foo],
+     *      то тег будет - foo.
+     *      По умолчанию: текущий тег
+     * @param array $atts
+     * @param string $content
+     * @param string $tag
+     * @return mixed
+     */
+    public function action($atts = array(), $content = '', $tag = '')
+    {
+        // TODO: Implement action() method.
+        /**
+         * Объединяет атрибуты (параметры) шоткода с известными атрибутами, остаются только известные
+         * атрибуты. Устанавливает значения атрибута по умолчанию, если он не указан.
+         */
+        $atts = shortcode_atts( array(
+            'currency' => 'RUB',
+            'origin' => '',
+            'destination' => '',
+            'month' => date('Y-m-d'),
+        ), $atts, $tag );
+        $reuestAPI = VdmnsRequestApi::getInstance();
+        $data = $reuestAPI->getCalendarPricesMonth($atts['currency'], $atts['origin'],
+            $atts['destination'], $atts['month']);
+        if ($data == false) return false;
+        return $this->render($data);
+    }
+
+    /**
+     * Функция отвечающа за вывод обработаной информации шорткодом
+     * @param $data
+     * @return mixed
+     */
+    public function render($data)
+    {
+        // TODO: Implement render() method.
+        var_dump($data);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
